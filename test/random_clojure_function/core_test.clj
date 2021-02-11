fb32091d8d8ffbdbdcc355f4141cb65095255338
Pete Looney <petelooney81@gmail.com>
2021-02-11 16:26:11 -0600

    Initial Commit - Random Clojure Function

---
+++ test/random_clojure_function/core_test.clj
@@ -0,0 +1,7 @@
+(ns random-clojure-function.core-test
+  (:require [clojure.test :refer :all]
+            [random-clojure-function.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
